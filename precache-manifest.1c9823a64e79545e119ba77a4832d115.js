6d5723f1344ce0541805833f5fff844004ea803c
ajayg415 <ajay.g415@gmail.com>
2020-06-21 19:15:35 +0530

    Updates

---
+++ precache-manifest.1c9823a64e79545e119ba77a4832d115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af04cf15f2c24f5f5b8cce6b0a9f6ca8",
+    "url": "/aboutMe/index.html"
+  },
+  {
+    "revision": "1e77231cbb273d832281",
+    "url": "/aboutMe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f83436c16a8069fc3d8",
+    "url": "/aboutMe/static/js/2.4ed63b3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aboutMe/static/js/2.4ed63b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e77231cbb273d832281",
+    "url": "/aboutMe/static/js/main.8b39a233.chunk.js"
+  },
+  {
+    "revision": "bbd192670b2fe1f66569",
+    "url": "/aboutMe/static/js/runtime-main.35ce44d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aboutMe/static/media/logo.5d5d9eef.svg"
+  }
+]);
